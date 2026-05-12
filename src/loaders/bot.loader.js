c0aeef077ceff8a01acad7f20ed6e65dcd66023c
vledinin <v.ledinin@gmail.com>
2026-05-12 22:28:30 +0300

    update bot.loader.js

--- src/loaders/bot.loader.js
+++ src/loaders/bot.loader.js
@@ -8,15 +8,3 @@ export function loadBot() {
 
     return bot
 }
-
-// import { Telegraf } from 'telegraf'
-//
-// export function loadBot() {
-//     const bot = new Telegraf(process.env.BOT_TOKEN)
-//
-//     bot.start((ctx) => {
-//         ctx.reply('Task Tracker Bot started')
-//     })
-//
-//     return bot
-// }
